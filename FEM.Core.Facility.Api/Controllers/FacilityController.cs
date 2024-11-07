2ef16e10bb7dda8fb04ab4894de6fb6e136bb29e
tinabenita <tina1717rego@gmail.com>
2024-11-07 10:00:31 +0530

    Uploaded controller code

---
+++ FEM.Core.Facility.Api/Controllers/FacilityController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FEM.Core.Facility.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacilityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetFacilities()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{FacilityId: guid}")]
+        public async Task<IActionResult> GetFacilityById(Guid FacilityId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("NameId")]
+        public async Task<IActionResult> GetFacilityNameIdPair()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFacility()
+        {
+            return Created();
+        }
+
+        [HttpPut("{FacilityId: guid}")]
+        public async Task<IActionResult> UpdateFacility(Guid FacilityId)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{FacilityId: guid}")]
+        public async Task<IActionResult> DeleteFacility(Guid FacilityId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FEM.Core.Facility.Domain/Facility.cs
@@ -0,0 +1,21 @@
+﻿namespace FEM.Core.Facility.Domain;
+
+/// <summary>
+/// Facility Domain Class
+/// </summary>
+/// <param name="FacilityId">Facility Id</param>
+/// <param name="Name">Name of the facility/building</param>
+/// <param name="StreetAddress">Street Address of the facility</param>
+/// <param name="City">City of the facility</param>
+/// <param name="Country">Country of the facility</param>
+/// <param name="Pincode">Pincode of the facility</param>
+/// <param name="OrgId">Organization Id</param>
+internal record Facility(
+    Guid FacilityId, 
+    string Name,  
+    string StreetAddress,
+    string City,
+    string Country,
+    string Pincode,
+    Guid OrgId
+    );
